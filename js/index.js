b90caa3cfe5d3537c496a91195c5e1d293833174
Nick Tk <nico.toccane@gmail.com>
2020-04-13 22:02:57 +0200

    complete note create and update features

--- js/index.js
+++ js/index.js
@@ -1,36 +1,46 @@
-const container = document.querySelector(".container");
+const noteContainer = document.querySelector(".container");
+
+// toggle from display none to block or viceversa
+function toggleVisibility(element){
+    const display = element.style.display;
+    element.style.display = display==="block" ? "none" : "block";
+}
+
+
+// get all the notes
 $.get(
     notesBaseUrl, 
     function(response){
         const { notes } = response;
         for(let i = 0; i<notes.length; i++){
-            const oldContent = container.innerHTML;
+            const note = notes[i];
+            const oldContent = noteContainer.innerHTML;
             const newContent = 
             `
-            <div class="note" id=${notes[i]._id}>
-                <h3 class="title">${ notes[i].body }</h3>
+            <div class="note" id=${note._id}>
+                <h3 class="title">${ note.body }</h3>
                 <button type="button" class="delete-note-btn">Delete Note</button>
                 <button type="button" class="edit-note-btn" id="">Edit</button>
-                <form action="${ notesBaseUrl }/${ notes[i]._id }" class="edit-note-form">
-                    <label for="note-${ notes[i]._id }-body">Your Note</label>
-                    <textarea name="body" id="note-${ notes[i]._id }-body" cols="30" rows="10">${ notes[i].body }</textarea>
-                    <label for="note-${ notes[i]._id }-pointed">Pointed</label>
-                    <input type="checkbox" name="pointed" id="note-${ notes[i]._id }-pointed">
+                <form action="${ notesBaseUrl }/${ note._id }" class="edit-note-form">
+                    <label for="note-${ note._id }-body">Your Note</label>
+                    <textarea name="body" id="note-${ note._id }-body" cols="30" rows="10">${ note.body }</textarea>
+                    <label for="note-${ note._id }-pointed">Pointed</label>
+                    <input type="checkbox" name="pointed" id="note-${ note._id }-pointed" ${ note.pointed ? "checked" : "" }>
                     <button type="submit">Update</button>
                 </form>
             </div>
             `;
-            container.innerHTML = oldContent + newContent;
+            noteContainer.innerHTML = oldContent + newContent;
         }        
     }
 );
 
-$(container).on("click", ".delete-note-btn", function(e){
+// delete a note
+$(noteContainer).on("click", ".delete-note-btn", function(e){
     e.preventDefault();
     e.stopPropagation();
     const res = confirm("Are you sure you want to delete this note?");
     if(res){
-        // delete the note
         const noteElement = this.parentElement;
         const id = noteElement.getAttribute("id");
         $.ajax({
@@ -45,13 +55,16 @@ $(container).on("click", ".delete-note-btn", function(e){
     }
 });
 
-$(container).on("click", ".edit-note-btn", function(e){
+// show or hide edit note btn
+$(noteContainer).on("click", ".edit-note-btn", function(e){
     e.preventDefault();
     e.stopPropagation();
-    $(this).siblings("form").toggle();
+    const form = this.nextElementSibling;
+    toggleVisibility(form);
 });
 
-$(container).on("submit", ".edit-note-form", function(e){
+// update a note
+$(noteContainer).on("submit", ".edit-note-form", function(e){
     e.preventDefault();
     e.stopPropagation();
     const data = $(this).serialize();
@@ -66,8 +79,51 @@ $(container).on("submit", ".edit-note-form", function(e){
             processData: false,
             data,
             success: function(response){
-                debugger;
+                const h3 = this.noteElement.children[0];
+                const form = this.noteElement.children[3];
+                h3.textContent = response.note.body;
+                form.style.display = "none";
             }
         }
     );
 });
+
+// show or hide note create form
+const createNoteBtn = document.getElementById("create-note-btn");
+createNoteBtn.addEventListener("click", function(e){
+    e.preventDefault();
+    const form = this.nextElementSibling;
+    toggleVisibility(form);
+});
+
+// create the note
+const createNoteForm = document.getElementById("create-note-form");
+createNoteForm.addEventListener("submit", function(e){
+    e.preventDefault();
+    const data = $(this).serialize();
+    $.post(notesBaseUrl, data, function(response){
+        const { note } = response;
+        const previousContent = noteContainer.innerHTML;
+        const newContent = 
+            `
+            <div class="note" id=${note._id}>
+                <h3 class="title">${ note.body }</h3>
+                <button type="button" class="delete-note-btn">Delete Note</button>
+                <button type="button" class="edit-note-btn" id="">Edit</button>
+                <form action="${ notesBaseUrl }/${ note._id }" class="edit-note-form">
+                    <label for="note-${ note._id }-body">Your Note</label>
+                    <textarea name="body" id="note-${ note._id }-body" cols="30" rows="10">${ note.body }</textarea>
+                    <label for="note-${ note._id }-pointed">Pointed</label>
+                    <input type="checkbox" name="pointed" id="note-${ note._id }-pointed" ${ note.pointed ? "checked" : "" }>
+                    <button type="submit">Update</button>
+                </form>
+            </div>
+            `;
+        noteContainer.innerHTML = newContent + previousContent;
+        const textarea = createNoteForm.children[1];
+        textarea.value = "";
+        const pointedCheckBox = createNoteForm.children[3];
+        pointedCheckBox.checked = false;
+        toggleVisibility(createNoteForm);
+    });
+})
