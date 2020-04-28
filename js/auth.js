2f7b41eb38319511750128b1e87693b607f7d044
Nick Tk <nico.toccane@gmail.com>
2020-04-28 20:57:39 +0200

    add regex phone number and email validation

--- js/auth.js
+++ js/auth.js
@@ -64,6 +64,8 @@ loginLink.addEventListener("click", function(e){
 });
 registrationForm.addEventListener("submit", function(e){
     e.preventDefault();
+    const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gi;
+    const phoneNumberRegex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/gi;
     const email = $(this).children("#registration-email").val();
     const username = $(this).children("#regisration-username").val();
     const password = $(this).children("#registration-password").val();
@@ -81,9 +83,10 @@ registrationForm.addEventListener("submit", function(e){
     } else {
         if(password!==passwordConfirm){
             coreMethods.setAlert("Passwords not matching", "danger");
-        } else if(!Number(phoneNumber)){
+        } else if(!phoneNumberRegex.exec(phoneNumber)){
             coreMethods.setAlert("Phone number not valid", "danger");
-            
+        } else if(!emailRegex.exec(email)){
+            coreMethods.setAlert("E-mail address not valid", "danger");
         } else {
             const data = $(this).serialize();
             const url = defaultUrl + "/register";
@@ -94,7 +97,10 @@ registrationForm.addEventListener("submit", function(e){
                 form: this,
                 success: function(response){
                     if(response.err){
-                        coreMethods.setAlert(response.err.message, "danger");
+                        let msg;
+                        if(response.err.message) msg = response.err.message;
+                        else msg = response.err;
+                        coreMethods.setAlert(msg, "danger");
                         $(this.form).children("input[type=password]").val("");
                     } else {
                         currentUser = response;
