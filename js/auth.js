4c8c1dec8a7936d7938c095e3becbb778f644c4e
Nick Tk <nico.toccane@gmail.com>
2020-05-04 21:44:07 +0200

    delete email field

--- js/auth.js
+++ js/auth.js
@@ -85,7 +85,6 @@ $twoFactorForm.submit(function(e){
 const registrationForm = document.getElementById("registration-form");
 const registrationLink = document.getElementById("registration-link");
 const loginLink = document.getElementById("login-link");
-const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i;
 const phoneNumberRegex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/i;
 
 registrationLink.addEventListener("click", function(e){
@@ -102,23 +101,10 @@ loginLink.addEventListener("click", function(e){
     $(loginForm).show();
 });
 
-const registrationEmailInput = document.getElementById("registration-email");
 const registrationPhoneInput = document.getElementById("registration-phone");
 const passwordInput = document.getElementById("registration-password");
 const paswordConfirmInput = document.getElementById("registration-password-confirm");
 const registrationFormSubmitBtn = document.querySelector("#registration-form > button[type=submit]");
-
-registrationEmailInput.addEventListener("input", function(e){
-    e.preventDefault();
-    const { value } = this;
-    if(!emailRegex.exec(value) && value.length){
-        coreMethods.setAlert("E-mail not valid", "danger");
-        registrationFormSubmitBtn.setAttribute("disabled", true);
-    } else {
-        coreMethods.setAlert();
-        registrationFormSubmitBtn.removeAttribute("disabled");
-    }
-});
 
 registrationPhoneInput.addEventListener("input", function(e){
     e.preventDefault();
@@ -152,14 +138,12 @@ paswordConfirmInput.addEventListener("input", function(e){
 
 registrationForm.addEventListener("submit", function(e){
     e.preventDefault();
-    const email = $(this).children("#registration-email").val();
     const username = $(this).children("#regisration-username").val();
     const password = $(this).children("#registration-password").val();
     const passwordConfirm = $(this).children("#registration-password-confirm").val();
     const phoneNumber = $(this).children("#registration-phone").val();
     let errMsg = "Missing ";
     if(!username) errMsg+="username, "
-    if(!email) errMsg+="E-mail, "
     if(!password) errMsg+="password, "
     if(!passwordConfirm) errMsg+="Password confirmation, "
     if(!phoneNumber) errMsg+="Phone Number, "
@@ -171,8 +155,6 @@ registrationForm.addEventListener("submit", function(e){
             coreMethods.setAlert("Passwords not matching", "danger");
         } else if(!phoneNumberRegex.exec(phoneNumber)){
             coreMethods.setAlert("Phone number not valid", "danger");
-        } else if(!emailRegex.exec(email)){
-            coreMethods.setAlert("E-mail address not valid", "danger");
         } else {
             const data = $(this).serialize();
             const url = defaultUrl + "/register";
